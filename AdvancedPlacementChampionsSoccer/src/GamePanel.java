4179d066776eefaf392e32d0a669b0ec2740c7aa
Brent <Brent@localhost>
2018-05-17 11:42:16 -0700

    timer

--- AdvancedPlacementChampionsSoccer/src/GamePanel.java
+++ AdvancedPlacementChampionsSoccer/src/GamePanel.java
@@ -20,9 +20,10 @@ public class GamePanel extends PApplet {
 	private int p1Score;
 	private int p2Score;
 	private PImage pauseButton;
-
-	//private SoundFile cheer;
-//	private boolean inJump;
+	private int time;
+	private int delay;
+	private boolean paused;
+	private int pauseDelay;
 
 	public GamePanel() {
 		ball = new Ball(700, 0, 30);
@@ -34,10 +35,10 @@ public class GamePanel extends PApplet {
 		rightGoal = new Goal(1120, 150, false,100, 400);
 		pauseButton = new PImage();
 		p1Score = 0;
-	
-	//	cheer = new SoundFile(this, "cheer.mp3");
-	//	inJump=false;
-		
+		time = 0;
+		delay = 0;
+		paused = false;
+		pauseDelay = 0;
 	}
 
 	public static void main(String[] args) {
@@ -47,10 +48,11 @@ public class GamePanel extends PApplet {
 	public void createBoundaries() {
 		//		boundaries[0] = new Surface(0, 0, (int) (3.0 * width / 4.0), (int) (3.0 * height / 5.0));
 		boundaries[0] = new Surface(0, (int) (height / 2.0) + 70,  width, (int) (height / 2.0));
-		
+
 	}
 
-	public void setup() {		
+	public void setup() {	
+		frame.setResizable(false);
 		background = loadImage("field.jpeg");
 		createBoundaries();
 		ball.setup(this);
@@ -60,8 +62,6 @@ public class GamePanel extends PApplet {
 		rightGoal.setup(this);
 		pauseButton = loadImage("pauseButton.png");
 		p2Score = 0;
-
-		
 		//leftGoal = new Goal((float)(width/25.6), (float)((height*3.0)/16.0), true,100, 400);
 	}
 
@@ -70,108 +70,114 @@ public class GamePanel extends PApplet {
 	}
 
 	public void draw() {			
-		clear();
-		
-		// draws everything onto the screen
-		leftGoal.setX((float)(width/25.6));
-		leftGoal.setY((float)((height*3.0)/16.0));
-		rightGoal.setX((float)((width*7.0)/8.0));
-		rightGoal.setY((float)((height*3.0)/16.0));
-		image(background, 0, 0, width, height);
-		image(pauseButton, width - 60, 10, 50, 50);
-		
-		p1.draw(this);
-		p2.draw(this);	
-		ball.draw(this);
-		rightGoal.draw(this);
-		leftGoal.draw(this);
+		if (!paused) {
+			time = millis();
+			if (delay == 0) 
+				delay = millis();
+			int displayTime = 60  - (time/1000)  + (delay/1000) + (pauseDelay/1000);
 
-		textSize(40);
-		text("SCORE: " + p1Score + " - " + p2Score, (float)(width/2.56),(float)((height*15.0)/16.0));
-		// creates physics between physics objects
+			if (displayTime >= 0) {
 
-		p1.act();
-		if (!p1.isOnSurface()) 
-			p1.fall(boundaries[0]);
+				clear();		
 
-		p2.act();
-		if (!p2.isOnSurface()) 
-			p2.fall(boundaries[0]);
+				// draws objects onto the screen
+				
+				leftGoal.setX((float)(width/25.6));
+				leftGoal.setY((float)((height*3.0)/16.0));
+				rightGoal.setX((float)((width*7.0)/8.0));
+				rightGoal.setY((float)((height*3.0)/16.0));
+				image(background, 0, 0, width, height);
+				image(pauseButton, width - 60, 10, 50, 50);
 
-		ball.act();
-		if (!ball.isOnSurface()) 
-			ball.fall(boundaries[0]);
-		if (ball.getVX() != 0)
-			ball.applyFriction();
+				p1.draw(this);
+				p2.draw(this);	
+				ball.draw(this);
+				rightGoal.draw(this);
+				leftGoal.draw(this);
 
-		//  collision detecting
-		
-		if (Math.abs(p1.getX() - p2.getX()) < 100 && Math.abs(p1.getY() - p2.getY()) < 135)
-			playerCollisionDetection();
-		else {
-			p1.setRightMovability(true);
-			p2.setLeftMovability(true);
+				// timing and scoring
+				
+				textSize(40);
+				fill(0);
+				if (displayTime >= 10)
+					text(displayTime, 12 * width / 25, height / 10);
+				else if (displayTime >= 0)
+					text(displayTime, 12 * width / 25, height / 10);
+				else
+					text("0", width / 2, height / 10);
+				fill(255);
+				text("SCORE: " + p1Score + " - " + p2Score, (float)(width/2.56),(float)((height*15.0)/16.0));
+
+				// creates physics between physics objects
+
+				p1.act();
+				if (!p1.isOnSurface()) 
+					p1.fall(boundaries[0]);
+
+				p2.act();
+				if (!p2.isOnSurface()) 
+					p2.fall(boundaries[0]);
+
+				ball.act();
+				if (!ball.isOnSurface()) 
+					ball.fall(boundaries[0]);
+				if (ball.getVX() != 0)
+					ball.applyFriction();
+
+				//  collision detecting
+
+				if (Math.abs(p1.getX() - p2.getX()) < 100 && Math.abs(p1.getY() - p2.getY()) < 135)
+					playerCollisionDetection();
+				else {
+					p1.setRightMovability(true);
+					p2.setLeftMovability(true);
+				}
+
+				if (Math.abs(p1.getX() - ball.getX()) < 150)
+					ballInteraction(p1);
+				if (Math.abs(p2.getX() - ball.getX()) < 150)
+					ballInteraction(p2);
+
+				if(Math.abs(p1.getY()+p1.getHeight()-ball.getY()) <5 && ball.getX()  + ball.getWidth() >p1.getX() && ball.getX()  <p1.getX()+p1.getWidth())
+				{
+					if(ball.getX() >= p1.getX() +p1.getHeight()/2)
+					{
+						ball.setX(p1.getX()+ p1.getWidth());
+					}
+					else
+					{
+						ball.setX(p1.getX());
+					}
+
+					ball.setVX(1.5* p1.getVX());
+				}
+				if(Math.abs(p2.getY()+p2.getHeight()-ball.getY()) <5 && ball.getX()  + ball.getWidth() >p2.getX() && ball.getX()  <p2.getX()+p2.getWidth())
+				{
+					if(ball.getX() >= p2.getX() +p2.getHeight()/2)
+					{
+						ball.setX(p2.getX()+ p2.getWidth());
+					}
+					else
+					{
+						ball.setX(p2.getX());
+					}
+
+					ball.setVX(1.5* p2.getVX());
+				}
+
+
+				goalInteraction();
+			}
+		} else {
+			pauseDelay = millis() - time;
 		}
-		
-		if (Math.abs(p1.getX() - ball.getX()) < 150)
-			ballInteraction(p1);
-		if (Math.abs(p2.getX() - ball.getX()) < 150)
-			ballInteraction(p2);
-//		if(inJump) {
-//			if(p1.getVY() == 0 && p2.getVY()==0)
-//			{
-//				inJump = false;
-//			}
-//		if(ball.getX() + ball.getWidth() >p1.getX() && ball.getX()<p1.getX()+p1.getWidth() && p1.getY()<ball.getY())
-//		{
-//			ball.setVX(1.5 * ball.getVX());
-//			//System.out.println("sd");
-//		}
-//		if(ball.getX()  + ball.getWidth() >p2.getX() && ball.getX()  <p2.getX()+p2.getWidth() && p1.getY()<ball.getY())
-//		{
-//			ball.setVX(1.5 * ball.getVX());
-//			//System.out.println("sd");
-//		}
-//		}
-		
-		if(Math.abs(p1.getY()+p1.getHeight()-ball.getY()) <5 && ball.getX()  + ball.getWidth() >p1.getX() && ball.getX()  <p1.getX()+p1.getWidth())
-		{
-			if(ball.getX() >= p1.getX() +p1.getHeight()/2)
-			{
-				ball.setX(p1.getX()+ p1.getWidth());
-			}
-			else
-			{
-				ball.setX(p1.getX());
-			}
-			
-			ball.setVX(1.5* p1.getVX());
-		}
-		if(Math.abs(p2.getY()+p2.getHeight()-ball.getY()) <5 && ball.getX()  + ball.getWidth() >p2.getX() && ball.getX()  <p2.getX()+p2.getWidth())
-		{
-			if(ball.getX() >= p2.getX() +p2.getHeight()/2)
-			{
-				ball.setX(p2.getX()+ p2.getWidth());
-			}
-			else
-			{
-				ball.setX(p2.getX());
-			}
-			
-			ball.setVX(1.5* p2.getVX());
-		}
-		
-		
-		goalInteraction();
-	//	System.out.println(width + "width");
-	//	System.out.println(height + "height");
 	}
 
 	public void keyPressed() {		
 		if (keyPressed) {
-			
+
 			// player 1
-			
+
 			if (key == 'a')
 				if (p1.canMoveLeft())
 					p1.walkHorizontally(-1);
@@ -180,7 +186,7 @@ public class GamePanel extends PApplet {
 					p1.walkHorizontally(1);
 			if (key == 'w') {
 				p1.jump();
-		//		inJump = true;
+				//		inJump = true;
 			}
 			if(key == 's')
 			{
@@ -199,7 +205,7 @@ public class GamePanel extends PApplet {
 					p2.walkHorizontally(1);
 			if (keyCode == UP) {
 				p2.jump();
-		//		inJump = true;
+				//		inJump = true;
 			}
 			if(keyCode == DOWN) {
 				if(ballInteraction(p2))
@@ -207,23 +213,26 @@ public class GamePanel extends PApplet {
 					p2.kick(ball, boundaries[0], false);
 				}
 			}
-			
+
 			if (key == ' ' ) {
 				System.out.print("rightX: " + p1.getX() + p1.getWidth());
 				System.out.print(", Player Width: " + p1.getWidth());
 				System.out.println(", BallX: " + ball.getX());
 			}
+			
+			if (key == 'p')
+				paused = !paused;
 		}	
 	}
-	
+
 	public void mousePressed()
 	{
 		if(mouseX>=width-60 && mouseY<=60 && mouseY>10 && mouseX<width-10)
 		{
 			//TONY ADD PAUSE SCREEN GRAPHIC
-			
+
 		}
-		
+
 	}
 
 	public void keyReleased() {
@@ -253,9 +262,9 @@ public class GamePanel extends PApplet {
 			p2.setLeftMovability(false);
 		}
 	}
-	
+
 	public boolean ballInteraction(Tekkist p) {
-		
+
 		if (p.getY() <= ball.getY() - ball.getHeight() && p.getY() + p.getHeight() >= ball.getY()) {
 			if (p.getX() + p.getWidth() >= ball.getX() && p.getX() + p.getWidth()/2.0 < ball.getX() ||
 					p.getX() <= ball.getX() + ball.getWidth() && p.getX() >= ball.getX() + ball.getWidth()/2.0) {
@@ -265,24 +274,24 @@ public class GamePanel extends PApplet {
 		}
 		return false;
 	}
-	
+
 	public void goalInteraction()
 	{
 		if(ball.getX()<=100+leftGoal.getX() && ball.getY() >= leftGoal.getY() && ball.getY()<=leftGoal.getY()+400)
 		{
 			p2Score++;
-	//		cheer.play();
+			//		cheer.play();
 			ball = new Ball(700, 0, 30);
 			ball.setup(this);
 		}
 		if(ball.getX()>=rightGoal.getX() && ball.getY() >= rightGoal.getY() && ball.getY()<=rightGoal.getY()+400)
 		{
 			p1Score++;
-	//		cheer.play();
+			//		cheer.play();
 			ball = new Ball(700, 0, 30);
 			ball.setup(this);
 		}
-		
+
 		if(p1.getX() + p1.getWidth() < leftGoal.getX()+100)
 		{
 			p1.setX(leftGoal.getX()+100-p1.getWidth());
@@ -299,8 +308,8 @@ public class GamePanel extends PApplet {
 		{
 			p2.setX(rightGoal.getX());
 		}
-		
+
 		//need to add bounce off crossbar
-		
+
 	}
 }
